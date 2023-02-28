100754b5f6208fa20740c3e660a4cdc6e76a776c
denys-natanael-clear <denys.natanael.clear@gmail.com>
2023-02-28 12:09:28 -0300

    feat: lessons 94: applying the functions for showing the progress of file upload, and showing how much time is left to upload the file.

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -5,6 +5,9 @@ class DropBoxController {
         this.btnSendFileEl = document.querySelector('#btn-send-file') 
         this.inputFilesEl = document.querySelector('#files')
         this.snackModalEl = document.querySelector('#react-snackbar-root')
+        this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg');
+        this.nameFileEl = this.snackModalEl.querySelector('.filename');
+        this.timeLeftEl = this.snackModalEl.querySelector('.timeleft');
 
         this.initEvents()
 
@@ -22,9 +25,17 @@ class DropBoxController {
 
             this.uploadTasks(event.target.files)
 
-            this.snackModalEl.style.display = 'block'
+            this.modalShow()
+
+            this.inputFilesEl.value = ''
 
         })
+
+    }
+
+    modalShow (show = true) {
+
+        this.snackModalEl.style.display = (show) ? 'block' : 'none'
 
     }
 
@@ -42,6 +53,8 @@ class DropBoxController {
 
                 ajax.onload = event => {
 
+                    this.modalShow(false)
+
                     try {
                         resolve(JSON.parse(ajax.responseText))
                     } catch (e) {
@@ -54,13 +67,23 @@ class DropBoxController {
 
                 ajax.onerror = event => {
 
+                    this.modalShow(false)
+
                     reject(event)
+
+                }
+
+                ajax.upload.onprogress = event => {
+
+                    this.uploadProgress(event, file)
 
                 }
 
                 let formData = new FormData()
 
                 formData.append('input-file', file)
+
+                this.startUploadTime = Date.now()
 
                 ajax.send(formData)
 
@@ -69,6 +92,42 @@ class DropBoxController {
         })
 
         return Promise.all(promises)
+
+    }
+
+    uploadProgress(event, file) {
+
+        let timespent = Date.now() - this.startUploadTime   
+        let loaded = event.loaded
+        let total = event.total
+        let porcent = parseInt((loaded / total) * 100)
+        let timeleft = ((100 - porcent) * timespent) / porcent
+
+        this.progressBarEl.style.width = `${porcent}%`
+
+        this.nameFileEl.innerHTML = file.name
+        this.timeLeftEl.innerHTML = this.formatTimeToHuman(timeleft)
+    }
+ 
+    formatTimeToHuman (duration) {
+
+        let seconds = parseInt((duration / 1000) % 60)
+        let minutes = parseInt((duration / (1000 * 60)) % 60)
+        let hours = parseInt((duration / (1000 * 60)) % 24)
+
+        if (hours > 0) {
+            return `${hours} horas, ${minutes} minutos e ${seconds} segundos`
+        }
+
+        if (minutes > 0) {
+            return `${minutes} minutos e ${seconds} segundos`
+        }
+
+        if (seconds > 0) {
+            return `${seconds} segundos`
+        }
+
+        return ''
 
     }
 
